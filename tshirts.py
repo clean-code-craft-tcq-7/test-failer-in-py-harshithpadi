0b5dcb5886f5e4fdce1bee820a21b5e2b6843c9f
HARSHITH PADI <115480440+harshithpadi@users.noreply.github.com>
2025-08-05 08:50:50 +0530

    Update tshirts.py

--- tshirts.py
+++ tshirts.py
@@ -7,9 +7,10 @@ def size(cms):
     else:
         return 'L'
 
-assert(size(37) == 'error')
+#assert(size(37) == 'error')
 assert(size(37) == 'S')
 assert(size(38) == 'M')
 assert(size(40) == 'M')
 assert(size(43) == 'L')
 print("All is well (maybe!)")
+
